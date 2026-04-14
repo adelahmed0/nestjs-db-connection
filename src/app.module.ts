60eed345242cc6d2b42c1336a349f8efae054258
Adel Ahmed <adel15952@gmail.com>
2026-04-14 23:44:07 +0200

    Add .editorconfig and integrate MongoDB module with ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { UsersModule } from './users/users.module';
 import { APP_PIPE } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
+import { MongoModule } from './mongo/mongo.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import * as Joi from 'joi';
       }),
     }),
     UsersModule,
+    MongoModule,
   ],
   providers: [{ provide: APP_PIPE, useClass: ValidationPipe }],
 })
